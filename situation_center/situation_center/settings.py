d29d69b111a0abc944c3e9a79eb68d171706a01a
Michael Florya <mih2303@mail.ru>
2024-03-30 21:01:16 +0300

    add app main

--- situation_center/situation_center/settings.py
+++ situation_center/situation_center/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Встроенные настройки
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Приложения
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
