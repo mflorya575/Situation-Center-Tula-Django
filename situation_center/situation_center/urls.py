53cbea00b00ee6024805ead4390773b6c4edbcc0
Michael Florya <mih2303@mail.ru>
2024-07-17 20:25:34 +0300

    config static files

--- situation_center/situation_center/urls.py
+++ situation_center/situation_center/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
-from main import views
+from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
