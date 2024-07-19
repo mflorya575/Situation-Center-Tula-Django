d4871bc8b8954cb8ca5abe5053ddcfa3be359213
Michael Florya <mih2303@mail.ru>
2024-07-20 00:51:23 +0300

    create system search info on tags

--- situation_center/news/views.py
+++ situation_center/news/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import News
+from taggit.models import Tag
 
 
 def news(request):
-    news_all = News.objects.all()
+    tag = request.GET.get('tag', None)
+
+    if tag:
+        news_all = News.objects.filter(tags__name=tag)
+    else:
+        news_all = News.objects.all()
+
+    # Получаем все теги, связанные с моделью News
+    tags = Tag.objects.all()
 
     context = {
         'news_all': news_all,
+        'tags': tags,
         'title': 'Новости | СЦ РЭУ филиал им. Г.В. Плеханова',
     }
 
