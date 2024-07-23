1968029a32245312989c60c975c20ee86501c477
Michael Florya <mih2303@mail.ru>
2024-07-23 20:54:04 +0300

    create page detail business

--- situation_center/nathprojects/urls.py
+++ situation_center/nathprojects/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('project-ecology/<slug:slug>/', views.ecology_detail, name='ecology_detail'),
     # Предпринимательство
     path('project-business/', views.business, name='business'),
+    path('project-business/<slug:slug>/', views.business_detail, name='business_detail'),
 ]
--- situation_center/nathprojects/views.py
+++ situation_center/nathprojects/views.py
@@ -450,3 +450,54 @@ def business(request):
         'title': 'Предпринимательство | СЦ РЭУ филиал им. Г.В. Плеханова',
     }
     return render(request, 'nathprojects/business.html', context)
+
+
+def business_detail(request, slug):
+    # Получаем конкретную запись из базы данных по slug
+    business = get_object_or_404(Business, slug=slug)
+    business_data = business.data.all().order_by('year')
+
+    selected_region = request.GET.get('region')
+    if selected_region:
+        business_data = business_data.filter(region=selected_region)
+
+    # Преобразуем данные в DataFrame для удобства работы с Plotly
+    df = pd.DataFrame(list(business_data.values()))
+
+    # Проверка наличия данных
+    if df.empty:
+        combined_chart = "Нет данных для отображения."
+    else:
+        # Создание комбинированного графика
+        fig = go.Figure()
+
+        # Добавление линейного графика
+        fig.add_trace(go.Scatter(x=df['year'], y=df['data'],
+                                 mode='lines+markers',
+                                 name='Линейный график'))
+
+        # Добавление столбчатой диаграммы
+        fig.add_trace(go.Bar(x=df['year'], y=df['data'],
+                             name='Столбчатая диаграмма'))
+
+        # Настройка осей и заголовка
+        fig.update_layout(
+            title=f'{business.title} - Комбинированный график',
+            xaxis_title='Годы',
+            yaxis_title='Количество'
+        )
+
+        # Преобразование графика в HTML
+        combined_chart = fig.to_html(full_html=False)
+
+    # Создание формы для выбора региона
+    region_form = RegionForm(request.GET or None)
+
+    context = {
+        'combined_chart': combined_chart,
+        'business': business,
+        'region_form': region_form,
+        'selected_region': dict(REGION_CHOICES).get(selected_region, 'Не выбран'),
+        'title': 'СЦ РЭУ филиал им. Г.В. Плеханова',
+    }
+    return render(request, 'nathprojects/business_detail.html', context)
