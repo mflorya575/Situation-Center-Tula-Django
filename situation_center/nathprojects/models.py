76dceef428b443ad4059535c3ef9f7f4123ee8e0
Michael Florya <mih2303@mail.ru>
2024-07-23 22:13:18 +0300

    create 2 models of Labour

--- situation_center/nathprojects/models.py
+++ situation_center/nathprojects/models.py
@@ -379,3 +379,36 @@ class WorldData(models.Model):
 
     def __str__(self):
         return f"{self.year} - {self.data} международки"
+
+
+class Labour(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название', blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    description = models.TextField(verbose_name='Описание', blank=True)
+
+    class Meta:
+        verbose_name = 'Труд'
+        verbose_name_plural = 'Труд'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title if self.title else 'Без названия'
+
+
+class LabourData(models.Model):
+    name = models.ForeignKey(Labour, related_name='data', on_delete=models.CASCADE)
+    year = models.IntegerField()
+    data = models.FloatField()
+    region = models.CharField(max_length=50, choices=REGION_CHOICES, verbose_name='Регион', blank=True)
+
+    class Meta:
+        verbose_name = 'Данные по труду'
+        verbose_name_plural = 'Данные по труду'
+        ordering = ['name']
+
+    def __str__(self):
+        return f"{self.year} - {self.data} труда"
