9e5c49925fb445f73d0a87f3a974bbfed590f6be
Michael Florya <mih2303@mail.ru>
2024-07-28 20:19:24 +0300

    create form for register users

--- situation_center/userauth/forms.py
+++ situation_center/userauth/forms.py
@@ -1,1 +1,34 @@
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from .models import MyUser
 
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = MyUser
+        fields = ('email', 'first_name', 'last_name')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Пароль не совпадает")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = MyUser
+        fields = ('email', 'password', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser')
