8db3613324c268b01e536a094b11b88e1b6b27fe
Michael Florya <mih2303@mail.ru>
2024-08-21 12:43:49 +0300

    create form of ecology

--- situation_center/nathprojects/forms.py
+++ situation_center/nathprojects/forms.py
@@ -206,3 +206,37 @@ class ScienceForm(forms.Form):
             # Обработка ошибок чтения файла или поиска больницы
             print(f"Ошибка: {e}")
             self.fields['region'].choices = [('Ошибка', 'Ошибка загрузки данных')]
+
+
+class EcologyForm(forms.Form):
+    region = forms.ChoiceField(
+        choices=[],  # Начальное значение, будет заменено в __init__
+        label='Выберите регион',
+        required=True,
+    )
+
+    def __init__(self, *args, **kwargs):
+        # Получаем slug больницы из kwargs
+        ecology_slug = kwargs.pop('ecology_slug')
+        super(EcologyForm, self).__init__(*args, **kwargs)
+
+        try:
+            # Найти науку по slug и загрузить CSV
+            ecology = Ecology.objects.get(slug=ecology_slug)
+            csv_file_path = ecology.csv_file.path
+            df = pd.read_csv(csv_file_path)
+
+            # Проверьте, что столбец 'region' присутствует
+            if 'region' not in df.columns:
+                raise ValueError("Столбец 'region' не найден в CSV-файле.")
+
+            # Получить уникальные регионы из CSV-файла
+            unique_regions = df['region'].unique()
+
+            # Обновить choices на основе данных из CSV
+            self.fields['region'].choices = [(region, region) for region in unique_regions]
+
+        except Exception as e:
+            # Обработка ошибок чтения файла или поиска больницы
+            print(f"Ошибка: {e}")
+            self.fields['region'].choices = [('Ошибка', 'Ошибка загрузки данных')]
