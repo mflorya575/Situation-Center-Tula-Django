a8afa1ff9e61bc03122837f3ffc7f2323580bb8a
Michael Florya <mih2303@mail.ru>
2024-08-21 12:47:17 +0300

    modern model Ecology & register in admin

--- situation_center/nathprojects/admin.py
+++ situation_center/nathprojects/admin.py
@@ -87,34 +87,18 @@ class ScienceAdmin(admin.ModelAdmin):
     )
 
 
-class EcologyDataInline(admin.TabularInline):
-    model = EcologyData
-    extra = 1
-
-
+@admin.register(Ecology)
 class EcologyAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug')
-    list_filter = ('title',)  # Фильтр по названию
-    search_fields = ('title', 'description')  # Поля для поиска
-    ordering = ['title']  # Сортировка по умолчанию
-    inlines = [EcologyDataInline]
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title',)
 
-
-class EcologyDataAdmin(admin.ModelAdmin):
-    list_display = ('name', 'year', 'data', 'region')
-    list_filter = ('name', 'year', 'region')  # Фильтр по больнице и году
-    search_fields = ('name__title', 'year', 'region')  # Поля для поиска
-    ordering = ['name']
-
-    def get_search_results(self, request, queryset, search_term):
-        """
-        Переопределение метода для улучшения поиска по полю name.
-        """
-        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
-        if search_term:
-            # Фильтруем по названию hospital, если есть поисковый запрос
-            queryset = queryset.filter(name__title__icontains=search_term)
-        return queryset, use_distinct
+    # Позволяет просматривать и загружать CSV файл
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'slug', 'description', 'csv_file')
+        }),
+    )
 
 
 class BusinessDataInline(admin.TabularInline):
@@ -295,16 +279,6 @@ class EconomDataAdmin(admin.ModelAdmin):
             # Фильтруем по названию hospital, если есть поисковый запрос
             queryset = queryset.filter(name__title__icontains=search_term)
         return queryset, use_distinct
-
-
-# Экология
-
-@admin.register(Ecology)
-class EcologyAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('title',)}
-
-
-admin.site.register(EcologyData, EcologyDataAdmin)
 
 
 # Предпринимательство
--- situation_center/nathprojects/models.py
+++ situation_center/nathprojects/models.py
@@ -142,6 +142,7 @@ class Ecology(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название', blank=True, null=True)
     slug = models.SlugField(unique=True, blank=True, null=True)
     description = models.TextField(verbose_name='Описание', blank=True)
+    csv_file = models.FileField(upload_to=csv_upload_to, verbose_name='CSV файл', blank=True, null=True)
 
     class Meta:
         verbose_name = 'Экология'
@@ -154,21 +155,6 @@ class Ecology(models.Model):
 
     def __str__(self):
         return self.title if self.title else 'Без названия'
-
-
-class EcologyData(models.Model):
-    name = models.ForeignKey(Ecology, related_name='data', on_delete=models.CASCADE)
-    year = models.IntegerField()
-    data = models.FloatField()
-    region = models.CharField(max_length=50, choices=REGION_CHOICES, verbose_name='Регион', blank=True)
-
-    class Meta:
-        verbose_name = 'Данные по экологии'
-        verbose_name_plural = 'Данные по экологии'
-        ordering = ['name']
-
-    def __str__(self):
-        return f"{self.year} - {self.data} экологии"
 
 
 class Business(models.Model):
