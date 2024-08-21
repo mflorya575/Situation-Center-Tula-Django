eb3def62e305bda7dd3fbde070f76640997ed922
Michael Florya <mih2303@mail.ru>
2024-08-21 13:12:10 +0300

    modern model Turism & register in admin

--- situation_center/nathprojects/admin.py
+++ situation_center/nathprojects/admin.py
@@ -115,34 +115,18 @@ class BusinessAdmin(admin.ModelAdmin):
     )
 
 
-class TurismDataInline(admin.TabularInline):
-    model = TurismData
-    extra = 1
-
-
+@admin.register(Turism)
 class TurismAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug')
-    list_filter = ('title',)  # Фильтр по названию
-    search_fields = ('title', 'description')  # Поля для поиска
-    ordering = ['title']  # Сортировка по умолчанию
-    inlines = [TurismDataInline]
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title',)
 
-
-class TurismDataAdmin(admin.ModelAdmin):
-    list_display = ('name', 'year', 'data', 'region')
-    list_filter = ('name', 'year', 'region')  # Фильтр по больнице и году
-    search_fields = ('name__title', 'year', 'region')  # Поля для поиска
-    ordering = ['name']
-
-    def get_search_results(self, request, queryset, search_term):
-        """
-        Переопределение метода для улучшения поиска по полю name.
-        """
-        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
-        if search_term:
-            # Фильтруем по названию hospital, если есть поисковый запрос
-            queryset = queryset.filter(name__title__icontains=search_term)
-        return queryset, use_distinct
+    # Позволяет просматривать и загружать CSV файл
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'slug', 'description', 'csv_file')
+        }),
+    )
 
 
 class HouseDataInline(admin.TabularInline):
@@ -263,16 +247,6 @@ class EconomDataAdmin(admin.ModelAdmin):
             # Фильтруем по названию hospital, если есть поисковый запрос
             queryset = queryset.filter(name__title__icontains=search_term)
         return queryset, use_distinct
-
-
-# Туризм
-
-@admin.register(Turism)
-class TurismAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('title',)}
-
-
-admin.site.register(TurismData, TurismDataAdmin)
 
 
 # Жилье
--- situation_center/nathprojects/forms.py
+++ situation_center/nathprojects/forms.py
@@ -274,3 +274,37 @@ class BusinessForm(forms.Form):
             # Обработка ошибок чтения файла или поиска больницы
             print(f"Ошибка: {e}")
             self.fields['region'].choices = [('Ошибка', 'Ошибка загрузки данных')]
+
+
+class TurismForm(forms.Form):
+    region = forms.ChoiceField(
+        choices=[],  # Начальное значение, будет заменено в __init__
+        label='Выберите регион',
+        required=True,
+    )
+
+    def __init__(self, *args, **kwargs):
+        # Получаем slug больницы из kwargs
+        turism_slug = kwargs.pop('turism_slug')
+        super(TurismForm, self).__init__(*args, **kwargs)
+
+        try:
+            # Найти науку по slug и загрузить CSV
+            turism = Turism.objects.get(slug=turism_slug)
+            csv_file_path = turism.csv_file.path
+            df = pd.read_csv(csv_file_path)
+
+            # Проверьте, что столбец 'region' присутствует
+            if 'region' not in df.columns:
+                raise ValueError("Столбец 'region' не найден в CSV-файле.")
+
+            # Получить уникальные регионы из CSV-файла
+            unique_regions = df['region'].unique()
+
+            # Обновить choices на основе данных из CSV
+            self.fields['region'].choices = [(region, region) for region in unique_regions]
+
+        except Exception as e:
+            # Обработка ошибок чтения файла или поиска больницы
+            print(f"Ошибка: {e}")
+            self.fields['region'].choices = [('Ошибка', 'Ошибка загрузки данных')]
