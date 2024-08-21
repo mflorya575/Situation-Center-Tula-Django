290deefe3fcaace0b660569a2874720ac351303d
Michael Florya <mih2303@mail.ru>
2024-08-21 13:20:19 +0300

    create form of house

--- situation_center/nathprojects/forms.py
+++ situation_center/nathprojects/forms.py
@@ -308,3 +308,37 @@ class TurismForm(forms.Form):
             # Обработка ошибок чтения файла или поиска больницы
             print(f"Ошибка: {e}")
             self.fields['region'].choices = [('Ошибка', 'Ошибка загрузки данных')]
+
+
+class HouseForm(forms.Form):
+    region = forms.ChoiceField(
+        choices=[],  # Начальное значение, будет заменено в __init__
+        label='Выберите регион',
+        required=True,
+    )
+
+    def __init__(self, *args, **kwargs):
+        # Получаем slug больницы из kwargs
+        house_slug = kwargs.pop('house_slug')
+        super(HouseForm, self).__init__(*args, **kwargs)
+
+        try:
+            # Найти науку по slug и загрузить CSV
+            house = House.objects.get(slug=house_slug)
+            csv_file_path = house.csv_file.path
+            df = pd.read_csv(csv_file_path)
+
+            # Проверьте, что столбец 'region' присутствует
+            if 'region' not in df.columns:
+                raise ValueError("Столбец 'region' не найден в CSV-файле.")
+
+            # Получить уникальные регионы из CSV-файла
+            unique_regions = df['region'].unique()
+
+            # Обновить choices на основе данных из CSV
+            self.fields['region'].choices = [(region, region) for region in unique_regions]
+
+        except Exception as e:
+            # Обработка ошибок чтения файла или поиска больницы
+            print(f"Ошибка: {e}")
+            self.fields['region'].choices = [('Ошибка', 'Ошибка загрузки данных')]
