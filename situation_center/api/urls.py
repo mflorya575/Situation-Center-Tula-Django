fad7195ce94f3d540177775c6a410962b297f74b
Michael Florya <mih2303@mail.ru>
2024-08-22 22:30:15 +0300

    add docs for api (redoc, swagger)

--- situation_center/api/urls.py
+++ situation_center/api/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
 from .views import *
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 
 app_name = 'api'
 
 urlpatterns = [
     path('hospital/<slug:slug>/', HospitalDetailAPIView.as_view(), name='hospital-detail-api'),
+
+    # Динамическая схема
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    # Документация
+    path('schema/redoc/', SpectacularRedocView.as_view(url='/api/schema/'), name='redoc'),
+    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url='/api/schema/'), name='swagger-ui'),
 ]
