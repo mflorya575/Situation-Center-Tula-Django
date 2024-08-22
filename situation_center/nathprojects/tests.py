54f24c5a62ceb8859466260e4cfa168fcc2e00b3
Michael Florya <mih2303@mail.ru>
2024-08-22 19:17:39 +0300

    create test for controller labour detail

--- situation_center/nathprojects/tests.py
+++ situation_center/nathprojects/tests.py
@@ -631,3 +631,60 @@ class WorldDetailViewTests(TestCase):
         response = client.get(reverse('nathprojects:world_detail', args=[self.world.slug]))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Ошибка при обработке данных")
+
+
+class LabourDetailViewTests(TestCase):
+
+    def setUp(self):
+        # Создание временного CSV-файла с корректными данными
+        csv_data = (
+            "region,2018,2019,2020,2021,2022\n"
+            "Российская Федерация,482.2,470,521.6,560,491.4\n"
+            "Центральный федеральный округ,446.7,435.2,496.4,543.2,454\n"
+            "Белгородская область,422.5,415.8,472.6,507.6,459.1\n"
+        )
+        self.temp_csv = tempfile.NamedTemporaryFile(delete=False, suffix=".csv", mode='w', newline='', encoding='utf-8')
+        self.temp_csv.write(csv_data)
+        self.temp_csv.close()
+
+        self.labour = Labour.objects.create(
+            title="Test Labour",
+            slug="test-labour",
+            csv_file=SimpleUploadedFile("test.csv", open(self.temp_csv.name, 'rb').read())
+        )
+
+    def tearDown(self):
+        self.temp_csv.close()
+
+    # Тест успешного получения страницы
+    def test_labour_detail_success(self):
+        client = Client()
+        response = client.get(reverse('nathprojects:labour_detail', args=[self.labour.slug]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Российская Федерация")
+
+    # Тест с выбранным регионом
+    def test_labour_detail_with_region(self):
+        client = Client()
+        response = client.get(reverse('nathprojects:labour_detail', args=[self.labour.slug]),
+                              {'region': 'Белгородская область'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Белгородская область")
+
+    # Тест, если регион не найден
+    def test_labour_detail_with_nonexistent_region(self):
+        client = Client()
+        response = client.get(reverse('nathprojects:labour_detail', args=[self.labour.slug]),
+                              {'region': 'Nonexistent Region'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Нет данных для отображения.")
+
+    # Тест обработки ошибок с некорректным CSV-файлом
+    def test_labour_detail_with_invalid_csv(self):
+        self.labour.csv_file = SimpleUploadedFile("test12.csv", b"invalid,data")
+        self.labour.save()
+
+        client = Client()
+        response = client.get(reverse('nathprojects:labour_detail', args=[self.labour.slug]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Ошибка при обработке данных")
