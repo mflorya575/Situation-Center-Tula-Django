302d76642fbe78a1d17cc9621a4fe511d674b54c
Michael Florya <mih2303@mail.ru>
2024-08-22 22:56:17 +0300

    create page industry

--- situation_center/operdata/urls.py
+++ situation_center/operdata/urls.py
@@ -7,5 +7,5 @@ app_name = 'operdata'
 
 urlpatterns = [
     # Промышленность
-    # path('operdata-industry/', views.industry, name='industry'),
+    path('operdata-industry/', views.industry, name='industry'),
 ]
--- situation_center/operdata/views.py
+++ situation_center/operdata/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+import plotly.express as px
+import pandas as pd
+import plotly.graph_objects as go
+import os
+import folium
+from folium.plugins import MarkerCluster
+
+from .models import *
 
 
+def industry(request):
+    # Получаем данные из базы данных и сортируем их по годам
+    industries = Industry.objects.all()
 
+    context = {
+        'industries': industries,
+        'title': 'Промышленность | СЦ РЭУ филиал им. Г.В. Плеханова',
+    }
+    return render(request, 'operdata/industry.html', context)
