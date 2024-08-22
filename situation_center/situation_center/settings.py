d84ed3b55b6c173d81d43a1016138a9d7ce45f1e
Michael Florya <mih2303@mail.ru>
2024-08-22 22:09:09 +0300

    add settings for drf

--- situation_center/situation_center/settings.py
+++ situation_center/situation_center/settings.py
@@ -110,6 +110,16 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+    ],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20
+}
+
 # Cache configuration
 CACHES = {
     'default': {
