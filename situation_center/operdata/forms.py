6d95f0f9cb23648f035ebe3faae55c72b12ac790
Michael Florya <mih2303@mail.ru>
2024-08-24 00:05:42 +0300

    create form prodprice

--- situation_center/operdata/forms.py
+++ situation_center/operdata/forms.py
@@ -308,3 +308,37 @@ class PriceForm(forms.Form):
             # Обработка ошибок чтения файла или поиска больницы
             print(f"Ошибка: {e}")
             self.fields['region'].choices = [('Ошибка', 'Ошибка загрузки данных')]
+
+
+class ProdPriceForm(forms.Form):
+    region = forms.ChoiceField(
+        choices=[],  # Начальное значение, будет заменено в __init__
+        label='Выберите регион',
+        required=True,
+    )
+
+    def __init__(self, *args, **kwargs):
+        # Получаем slug больницы из kwargs
+        prodprice_slug = kwargs.pop('prodprice_slug')
+        super(ProdPriceForm, self).__init__(*args, **kwargs)
+
+        try:
+            # Найти цену производителей по slug и загрузить CSV
+            prodprice = ProdPrice.objects.get(slug=prodprice_slug)
+            csv_file_path = prodprice.csv_file.path
+            df = pd.read_csv(csv_file_path)
+
+            # Проверьте, что столбец 'region' присутствует
+            if 'region' not in df.columns:
+                raise ValueError("Столбец 'region' не найден в CSV-файле.")
+
+            # Получить уникальные регионы из CSV-файла
+            unique_regions = df['region'].unique()
+
+            # Обновить choices на основе данных из CSV
+            self.fields['region'].choices = [(region, region) for region in unique_regions]
+
+        except Exception as e:
+            # Обработка ошибок чтения файла или поиска больницы
+            print(f"Ошибка: {e}")
+            self.fields['region'].choices = [('Ошибка', 'Ошибка загрузки данных')]
