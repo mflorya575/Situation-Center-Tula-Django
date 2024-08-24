07ba728f6eef545beaf05a1c37aab8bc689c4300
Michael Florya <mih2303@mail.ru>
2024-08-24 12:08:44 +0300

    create model Popelation

--- situation_center/yeardata/models.py
+++ situation_center/yeardata/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils.text import slugify
+import os
 
-# Create your models here.
+
+def csv_upload_to(instance, filename):
+    # Генерация пути для сохранения файла
+    return os.path.join('csv_datasets_yeardata', f'{instance.slug}', filename)
+
+
+class Population(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название', blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    description = models.TextField(verbose_name='Описание', blank=True)
+    csv_file = models.FileField(upload_to=csv_upload_to, verbose_name='CSV файл', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Население'
+        verbose_name_plural = 'Население'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title if self.title else 'Без названия'
