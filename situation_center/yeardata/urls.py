f7a08e818ca1ea98177944dc20b32fd158caf529
Michael Florya <mih2303@mail.ru>
2024-08-24 13:01:39 +0300

    create page population

--- situation_center/yeardata/urls.py
+++ situation_center/yeardata/urls.py
@@ -6,5 +6,5 @@ from . import views
 app_name = 'yeardata'
 
 urlpatterns = [
-
+    path('yeardata-population/', views.population, name='population'),
 ]
--- situation_center/yeardata/views.py
+++ situation_center/yeardata/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+import plotly.express as px
+import pandas as pd
+import plotly.graph_objects as go
+import os
+import folium
+from folium.plugins import MarkerCluster
 
-# Create your views here.
+from .models import *
+
+
+def population(request):
+    # Получаем данные из базы данных и сортируем их по годам
+    populations = Population.objects.all()
+
+    context = {
+        'populations': populations,
+        'title': 'Население | СЦ РЭУ филиал им. Г.В. Плеханова',
+    }
+    return render(request, 'yeardata/population.html', context)
