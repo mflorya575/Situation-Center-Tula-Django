cf362b3fb8ddb31eaadd15c1b484d953cfc8bc62
Michael Florya <mih2303@mail.ru>
2024-08-29 12:51:08 +0300

    change js mask in page index

--- situation_center/static/assets/js/main.js
+++ situation_center/static/assets/js/main.js
@@ -124,70 +124,41 @@ window.onload = function() {
 
 
 /* Маска для номера телефона */
-window.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll('.tel').forEach(function(input) {
-      var keyCode;
-  
-      function mask(event) {
-        event.keyCode && (keyCode = event.keyCode);
-        var pos = this.selectionStart;
-  
-        if (pos < 3 && event.keyCode !== 8 && event.keyCode !== 46) { // Allow backspace (8) and delete (46) keys
-          event.preventDefault();
-        }
-  
-        var matrix = "+7 (___) ___ ____",
-            i = 0,
-            def = matrix.replace(/\D/g, ""),
-            val = this.value.replace(/\D/g, ""),
-            new_value = matrix.replace(/[_\d]/g, function(a) {
-                return i < val.length ? val.charAt(i++) : a;
-            });
-  
-        i = new_value.indexOf("_");
-        if (i != -1) {
-            i < 5 && (i = 3);
-            new_value = new_value.slice(0, i);
-        }
-  
-        var reg = matrix.substr(0, this.value.length).replace(/_+/g, function(a) {
-            return "\\d{1," + a.length + "}";
-        }).replace(/[+()]/g, "\\$&");
-  
-        reg = new RegExp("^" + reg + "$");
-  
-        if (!reg.test(this.value) || this.value.length < 5 || (keyCode > 47 && keyCode < 58)) {
-          this.value = new_value;
-        }
-  
-        if (event.type == "blur" && this.value.length < 5) {
-          this.value = "";
-        }
-  
-        // Fix cursor position
-        if (pos < this.value.length) {
-          this.setSelectionRange(pos, pos);
-        }
-      }
-  
-      function handleDelete(event) {
-        if (this.selectionStart == 0 && this.selectionEnd == this.value.length) {
-          this.value = "";
-        }
-      }
-  
-      function handleInput(event) {
-        if (this.value === "") {
-          this.value = "+7 ";
-        }
-      }
-  
-      input.addEventListener("input", mask, false);
-      input.addEventListener("focus", mask, false);
-      input.addEventListener("blur", mask, false);
-      input.addEventListener("keydown", mask, false);
-      input.addEventListener("keydown", handleDelete, false);
-      input.addEventListener("input", handleInput, false);
-  
-    });
-  });
+function maskPhone(selector, masked = '+7 (___) ___-__-__') {
+	const elems = document.querySelectorAll(selector);
+
+	function mask(event) {
+		const keyCode = event.keyCode;
+		const template = masked,
+			def = template.replace(/\D/g, ""),
+			val = this.value.replace(/\D/g, "");
+		console.log(template);
+		let i = 0,
+			newValue = template.replace(/[_\d]/g, function (a) {
+				return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+			});
+		i = newValue.indexOf("_");
+		if (i !== -1) {
+			newValue = newValue.slice(0, i);
+		}
+		let reg = template.substr(0, this.value.length).replace(/_+/g,
+			function (a) {
+				return "\\d{1," + a.length + "}";
+			}).replace(/[+()]/g, "\\$&");
+		reg = new RegExp("^" + reg + "$");
+		if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+			this.value = newValue;
+		}
+		if (event.type === "blur" && this.value.length < 5) {
+			this.value = "";
+		}
+
+	}
+
+	for (const elem of elems) {
+		elem.addEventListener("input", mask);
+		elem.addEventListener("focus", mask);
+		elem.addEventListener("blur", mask);
+	}
+	
+}
