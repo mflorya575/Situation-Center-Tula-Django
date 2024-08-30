81a14ce7818ee35cef4916e938d763d6f3f4bc65
Michael Florya <mih2303@mail.ru>
2024-08-30 23:33:07 +0300

    create api world

--- situation_center/api/serializers.py
+++ situation_center/api/serializers.py
@@ -124,3 +124,15 @@ class HouseDetailSerializer(serializers.ModelSerializer):
         model = House
         fields = ['id', 'title', 'slug', 'combined_chart_linear', 'combined_chart_log', 'map_chart_linear', 'map_chart_log', 'table_html']
 
+
+class WorldDetailSerializer(serializers.ModelSerializer):
+    combined_chart_linear = serializers.CharField()
+    combined_chart_log = serializers.CharField()
+    map_chart_linear = serializers.CharField()
+    map_chart_log = serializers.CharField()
+    table_html = serializers.CharField()
+
+    class Meta:
+        model = World
+        fields = ['id', 'title', 'slug', 'combined_chart_linear', 'combined_chart_log', 'map_chart_linear', 'map_chart_log', 'table_html']
+
--- situation_center/api/urls.py
+++ situation_center/api/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path('business/<slug:slug>/', BusinessDetailAPIView.as_view(), name='business-detail-api'),
     path('turism/<slug:slug>/', TurismDetailAPIView.as_view(), name='turism-detail-api'),
     path('house/<slug:slug>/', HouseDetailAPIView.as_view(), name='house-detail-api'),
+    path('world/<slug:slug>/', WorldDetailAPIView.as_view(), name='world-detail-api'),
 
     # Динамическая схема
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
