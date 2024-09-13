20cc4da04178c201ea0a2fd0d1bf71ba1205fff1
Michael Florya <mih2303@mail.ru>
2024-09-13 12:14:23 +0300

    create model Turism

--- situation_center/foresttrees/models.py
+++ situation_center/foresttrees/models.py
@@ -157,3 +157,22 @@ class Business(models.Model):
 
     def __str__(self):
         return self.title if self.title else 'Без названия'
+
+
+class Turism(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название', blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    description = models.TextField(verbose_name='Описание', blank=True)
+    csv_file = models.FileField(upload_to=csv_upload_to, verbose_name='CSV файл', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Туризм'
+        verbose_name_plural = 'Туризм'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title if self.title else 'Без названия'
