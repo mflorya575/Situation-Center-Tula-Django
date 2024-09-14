6a08c905bb3400d9cfe5b7d01a5d74b8c6241e00
Michael Florya <mih2303@mail.ru>
2024-09-15 00:11:15 +0300

    create page of building

--- situation_center/activeforecast/urls.py
+++ situation_center/activeforecast/urls.py
@@ -42,4 +42,6 @@ urlpatterns = [
     path('industry/', views.industry, name='industry'),
     # Сельское хозяйство
     path('agro/', views.agro, name='agro'),
+    # Строительство
+    path('building/', views.building, name='building'),
 ]
