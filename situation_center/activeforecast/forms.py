0877677e91b178003b456c390bad144554b95e65
Michael Florya <mih2303@mail.ru>
2024-09-15 16:57:38 +0300

    create form of atom

--- situation_center/activeforecast/forms.py
+++ situation_center/activeforecast/forms.py
@@ -410,3 +410,37 @@ class LabourForm(forms.Form):
             # Обработка ошибок чтения файла или поиска больницы
             print(f"Ошибка: {e}")
             self.fields['region'].choices = [('Ошибка', 'Ошибка загрузки данных')]
+
+
+class AtomForm(forms.Form):
+    region = forms.ChoiceField(
+        choices=[],  # Начальное значение, будет заменено в __init__
+        label='Выберите регион',
+        required=True,
+    )
+
+    def __init__(self, *args, **kwargs):
+        # Получаем slug больницы из kwargs
+        atom_slug = kwargs.pop('atom_slug')
+        super(AtomForm, self).__init__(*args, **kwargs)
+
+        try:
+            # Найти больницу по slug и загрузить CSV
+            atom = Atom.objects.get(slug=atom_slug)
+            csv_file_path = atom.csv_file.path
+            df = pd.read_csv(csv_file_path)
+
+            # Проверьте, что столбец 'region' присутствует
+            if 'region' not in df.columns:
+                raise ValueError("Столбец 'region' не найден в CSV-файле.")
+
+            # Получить уникальные регионы из CSV-файла
+            unique_regions = df['region'].unique()
+
+            # Обновить choices на основе данных из CSV
+            self.fields['region'].choices = [(region, region) for region in unique_regions]
+
+        except Exception as e:
+            # Обработка ошибок чтения файла или поиска больницы
+            print(f"Ошибка: {e}")
+            self.fields['region'].choices = [('Ошибка', 'Ошибка загрузки данных')]
