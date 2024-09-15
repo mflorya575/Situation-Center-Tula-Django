a7c0b623a6d73dd9082b63a8aa40cbb44c8b8386
Michael Florya <mih2303@mail.ru>
2024-09-15 18:16:43 +0300

    create form of trading

--- situation_center/activeforecast/forms.py
+++ situation_center/activeforecast/forms.py
@@ -648,3 +648,37 @@ class TransportForm(forms.Form):
             # Обработка ошибок чтения файла или поиска больницы
             print(f"Ошибка: {e}")
             self.fields['region'].choices = [('Ошибка', 'Ошибка загрузки данных')]
+
+
+class TradingForm(forms.Form):
+    region = forms.ChoiceField(
+        choices=[],  # Начальное значение, будет заменено в __init__
+        label='Выберите регион',
+        required=True,
+    )
+
+    def __init__(self, *args, **kwargs):
+        # Получаем slug больницы из kwargs
+        trading_slug = kwargs.pop('trading_slug')
+        super(TradingForm, self).__init__(*args, **kwargs)
+
+        try:
+            # Найти больницу по slug и загрузить CSV
+            trading = Trading.objects.get(slug=trading_slug)
+            csv_file_path = trading.csv_file.path
+            df = pd.read_csv(csv_file_path)
+
+            # Проверьте, что столбец 'region' присутствует
+            if 'region' not in df.columns:
+                raise ValueError("Столбец 'region' не найден в CSV-файле.")
+
+            # Получить уникальные регионы из CSV-файла
+            unique_regions = df['region'].unique()
+
+            # Обновить choices на основе данных из CSV
+            self.fields['region'].choices = [(region, region) for region in unique_regions]
+
+        except Exception as e:
+            # Обработка ошибок чтения файла или поиска больницы
+            print(f"Ошибка: {e}")
+            self.fields['region'].choices = [('Ошибка', 'Ошибка загрузки данных')]
