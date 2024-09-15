e5c62d4b62a5f78975936d88a5fe3aaab642258c
Michael Florya <mih2303@mail.ru>
2024-09-15 22:42:21 +0300

    create form of capitalassets

--- situation_center/activeforecast/forms.py
+++ situation_center/activeforecast/forms.py
@@ -1124,3 +1124,37 @@ class SecureNatureForm(forms.Form):
             # Обработка ошибок чтения файла или поиска больницы
             print(f"Ошибка: {e}")
             self.fields['region'].choices = [('Ошибка', 'Ошибка загрузки данных')]
+
+
+class CapitalAssetsForm(forms.Form):
+    region = forms.ChoiceField(
+        choices=[],  # Начальное значение, будет заменено в __init__
+        label='Выберите регион',
+        required=True,
+    )
+
+    def __init__(self, *args, **kwargs):
+        # Получаем slug больницы из kwargs
+        capitalassets_slug = kwargs.pop('capitalassets_slug')
+        super(CapitalAssetsForm, self).__init__(*args, **kwargs)
+
+        try:
+            # Найти больницу по slug и загрузить CSV
+            capitalassets = CapitalAssets.objects.get(slug=capitalassets_slug)
+            csv_file_path = capitalassets.csv_file.path
+            df = pd.read_csv(csv_file_path)
+
+            # Проверьте, что столбец 'region' присутствует
+            if 'region' not in df.columns:
+                raise ValueError("Столбец 'region' не найден в CSV-файле.")
+
+            # Получить уникальные регионы из CSV-файла
+            unique_regions = df['region'].unique()
+
+            # Обновить choices на основе данных из CSV
+            self.fields['region'].choices = [(region, region) for region in unique_regions]
+
+        except Exception as e:
+            # Обработка ошибок чтения файла или поиска больницы
+            print(f"Ошибка: {e}")
+            self.fields['region'].choices = [('Ошибка', 'Ошибка загрузки данных')]
