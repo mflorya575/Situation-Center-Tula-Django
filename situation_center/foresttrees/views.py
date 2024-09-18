eb049812bea0e5700d0aec7e0ca7bfe2464110b4
Michael Florya <mih2303@mail.ru>
2024-09-18 16:06:22 +0300

    create page demographics detail

--- situation_center/foresttrees/views.py
+++ situation_center/foresttrees/views.py
@@ -102,6 +102,39 @@ def demographics(request):
     return render(request, 'foresttrees/demographics.html', context)
 
 
+def demographics_view(request, slug):
+    demographics = get_object_or_404(Demographics, slug=slug)
+
+    if request.method == "POST":
+        target_column = request.POST.get("target_column")
+        feature_columns = request.POST.getlist("feature_columns")
+
+        data = process_csv_data(demographics.csv_file.path)
+        result = calculate_random_forest(data, target_column, feature_columns)
+
+        context = {
+            "demographics": demographics,
+            "train_r2_score": result["train_r2_score"],
+            "test_r2_score": result["test_r2_score"],
+            "train_mse": result["train_mse"],
+            "test_mse": result["test_mse"],
+            "feature_importances": zip(feature_columns, result["feature_importances"]),
+            "trees": result["trees"],
+            "feature_importances_plot": result["feature_importances_plot"],
+            'title': 'СЦ РЭУ филиал им. Г.В. Плеханова',
+        }
+        return render(request, "foresttrees/demographics_result.html", context)
+
+    data = process_csv_data(demographics.csv_file.path)
+    columns = data.columns
+
+    return render(request, "foresttrees/demographics_detail.html", {
+        "demographics": demographics,
+        "columns": columns,
+        'title': 'СЦ РЭУ филиал им. Г.В. Плеханова',
+    })
+
+
 def culture(request):
     cultures = Culture.objects.all()
 
