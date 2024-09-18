35d69a5845f73ba2dbf56b08e6211e1eb7c912f6
Michael Florya <mih2303@mail.ru>
2024-09-18 16:12:35 +0300

    create page culture detail

--- situation_center/foresttrees/views.py
+++ situation_center/foresttrees/views.py
@@ -146,6 +146,39 @@ def culture(request):
     return render(request, 'foresttrees/culture.html', context)
 
 
+def culture_view(request, slug):
+    culture = get_object_or_404(Culture, slug=slug)
+
+    if request.method == "POST":
+        target_column = request.POST.get("target_column")
+        feature_columns = request.POST.getlist("feature_columns")
+
+        data = process_csv_data(culture.csv_file.path)
+        result = calculate_random_forest(data, target_column, feature_columns)
+
+        context = {
+            "culture": culture,
+            "train_r2_score": result["train_r2_score"],
+            "test_r2_score": result["test_r2_score"],
+            "train_mse": result["train_mse"],
+            "test_mse": result["test_mse"],
+            "feature_importances": zip(feature_columns, result["feature_importances"]),
+            "trees": result["trees"],
+            "feature_importances_plot": result["feature_importances_plot"],
+            'title': 'СЦ РЭУ филиал им. Г.В. Плеханова',
+        }
+        return render(request, "foresttrees/culture_result.html", context)
+
+    data = process_csv_data(culture.csv_file.path)
+    columns = data.columns
+
+    return render(request, "foresttrees/culture_detail.html", {
+        "culture": culture,
+        "columns": columns,
+        'title': 'СЦ РЭУ филиал им. Г.В. Плеханова',
+    })
+
+
 def road(request):
     roads = Road.objects.all()
 
