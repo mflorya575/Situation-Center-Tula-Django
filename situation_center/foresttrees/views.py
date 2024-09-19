ae23fa96fd9cae5640aa8d45297dd9da997262df
Michael Florya <mih2303@mail.ru>
2024-09-19 12:15:11 +0300

    create page ecology detail

--- situation_center/foresttrees/views.py
+++ situation_center/foresttrees/views.py
@@ -278,6 +278,39 @@ def ecology(request):
     return render(request, 'foresttrees/ecology.html', context)
 
 
+def ecology_view(request, slug):
+    ecology = get_object_or_404(Ecology, slug=slug)
+
+    if request.method == "POST":
+        target_column = request.POST.get("target_column")
+        feature_columns = request.POST.getlist("feature_columns")
+
+        data = process_csv_data(ecology.csv_file.path)
+        result = calculate_random_forest(data, target_column, feature_columns)
+
+        context = {
+            "ecology": ecology,
+            "train_r2_score": result["train_r2_score"],
+            "test_r2_score": result["test_r2_score"],
+            "train_mse": result["train_mse"],
+            "test_mse": result["test_mse"],
+            "feature_importances": zip(feature_columns, result["feature_importances"]),
+            "trees": result["trees"],
+            "feature_importances_plot": result["feature_importances_plot"],
+            'title': 'СЦ РЭУ филиал им. Г.В. Плеханова',
+        }
+        return render(request, "foresttrees/ecology_result.html", context)
+
+    data = process_csv_data(ecology.csv_file.path)
+    columns = data.columns
+
+    return render(request, "foresttrees/ecology_detail.html", {
+        "ecology": ecology,
+        "columns": columns,
+        'title': 'СЦ РЭУ филиал им. Г.В. Плеханова',
+    })
+
+
 def business(request):
     businesses = Business.objects.all()
 
