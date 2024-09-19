274b43f1413d865d12236122af0d1cb456c66c69
Michael Florya <mih2303@mail.ru>
2024-09-20 00:08:57 +0300

    create page finance detail

--- situation_center/foresttrees/views.py
+++ situation_center/foresttrees/views.py
@@ -1642,6 +1642,39 @@ def finance(request):
     return render(request, 'foresttrees/finance.html', context)
 
 
+def finance_view(request, slug):
+    finance = get_object_or_404(Finance, slug=slug)
+
+    if request.method == "POST":
+        target_column = request.POST.get("target_column")
+        feature_columns = request.POST.getlist("feature_columns")
+
+        data = process_csv_data(finance.csv_file.path)
+        result = calculate_random_forest(data, target_column, feature_columns)
+
+        context = {
+            "finance": finance,
+            "train_r2_score": result["train_r2_score"],
+            "test_r2_score": result["test_r2_score"],
+            "train_mse": result["train_mse"],
+            "test_mse": result["test_mse"],
+            "feature_importances": zip(feature_columns, result["feature_importances"]),
+            "trees": result["trees"],
+            "feature_importances_plot": result["feature_importances_plot"],
+            'title': 'СЦ РЭУ филиал им. Г.В. Плеханова',
+        }
+        return render(request, "foresttrees/finance_result.html", context)
+
+    data = process_csv_data(finance.csv_file.path)
+    columns = data.columns
+
+    return render(request, "foresttrees/finance_detail.html", {
+        "finance": finance,
+        "columns": columns,
+        'title': 'СЦ РЭУ филиал им. Г.В. Плеханова',
+    })
+
+
 def foreigntrading(request):
     foreigntradings = ForeignTrading.objects.all()
 
