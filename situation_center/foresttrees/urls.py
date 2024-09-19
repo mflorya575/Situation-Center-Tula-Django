87bd317d0400c8a53f217b37d221455e3ea6f59a
Michael Florya <mih2303@mail.ru>
2024-09-20 00:16:21 +0300

    create page industrialprod detail

--- situation_center/foresttrees/urls.py
+++ situation_center/foresttrees/urls.py
@@ -128,4 +128,5 @@ urlpatterns = [
     path('vrp/<slug:slug>/', views.vrp_view, name='vrp_view'),
     # Промышленное производство
     path('industrialprod/', views.industrialprod, name='industrialprod'),
+    path('industrialprod/<slug:slug>/', views.industrialprod_view, name='industrialprod_view'),
 ]
