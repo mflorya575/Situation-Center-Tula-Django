072fb7eb2e6051b48ace1496f33dadf18a59fa05
Michael Florya <mih2303@mail.ru>
2024-09-21 16:12:35 +0300

    create page world detail

--- situation_center/ai/views.py
+++ situation_center/ai/views.py
@@ -474,6 +474,40 @@ def world(request):
     return render(request, 'ai/world.html', context)
 
 
+def world_view(request, slug):
+    world = get_object_or_404(World, slug=slug)
+
+    if request.method == "POST":
+        target_column = request.POST.get("target_column")
+        feature_columns = request.POST.getlist("feature_columns")
+
+        data = process_csv_data(world.csv_file.path)
+        result = calculate_random_forest(data, target_column, feature_columns)
+
+        context = {
+            "world": world,
+            "train_r2_score": result["train_r2_score"],
+            "test_r2_score": result["test_r2_score"],
+            "train_mse": result["train_mse"],
+            "test_mse": result["test_mse"],
+            "feature_importances": zip(feature_columns, result["feature_importances"]),
+            "trees": result["trees"],
+            "feature_importances_plot": result["feature_importances_plot"],
+            "model_filename": result["model_filename"],
+            'title': 'СЦ РЭУ филиал им. Г.В. Плеханова',
+        }
+        return render(request, "ai/world_result.html", context)
+
+    data = process_csv_data(world.csv_file.path)
+    columns = data.columns
+
+    return render(request, "ai/world_detail.html", {
+        "world": world,
+        "columns": columns,
+        'title': 'СЦ РЭУ филиал им. Г.В. Плеханова',
+    })
+
+
 def labour(request):
     labours = Labour.objects.all()
 
