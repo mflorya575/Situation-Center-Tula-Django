0d9ac1db91eb45fd7c75486c64a68a6236b87f77
Michael Florya <mih2303@mail.ru>
2024-09-23 00:21:41 +0300

    create page price detail

--- situation_center/ai/views.py
+++ situation_center/ai/views.py
@@ -1059,6 +1059,40 @@ def price(request):
     return render(request, 'ai/price.html', context)
 
 
+def price_view(request, slug):
+    price = get_object_or_404(Price, slug=slug)
+
+    if request.method == "POST":
+        target_column = request.POST.get("target_column")
+        feature_columns = request.POST.getlist("feature_columns")
+
+        data = process_csv_data(price.csv_file.path)
+        result = calculate_random_forest(data, target_column, feature_columns)
+
+        context = {
+            "price": price,
+            "train_r2_score": result["train_r2_score"],
+            "test_r2_score": result["test_r2_score"],
+            "train_mse": result["train_mse"],
+            "test_mse": result["test_mse"],
+            "feature_importances": zip(feature_columns, result["feature_importances"]),
+            "trees": result["trees"],
+            "feature_importances_plot": result["feature_importances_plot"],
+            "model_filename": result["model_filename"],
+            'title': 'СЦ РЭУ филиал им. Г.В. Плеханова',
+        }
+        return render(request, "ai/price_result.html", context)
+
+    data = process_csv_data(price.csv_file.path)
+    columns = data.columns
+
+    return render(request, "ai/price_detail.html", {
+        "price": price,
+        "columns": columns,
+        'title': 'СЦ РЭУ филиал им. Г.В. Плеханова',
+    })
+
+
 def prodprice(request):
     prodprices = ProdPrice.objects.all()
 
