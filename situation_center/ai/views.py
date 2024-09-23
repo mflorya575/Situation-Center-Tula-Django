5619a28699931d72a2cea268ace66722733b9b99
Michael Florya <mih2303@mail.ru>
2024-09-23 15:23:32 +0300

    create page organization detail

--- situation_center/ai/views.py
+++ situation_center/ai/views.py
@@ -1554,6 +1554,40 @@ def organization(request):
     return render(request, 'ai/organization.html', context)
 
 
+def organization_view(request, slug):
+    organization = get_object_or_404(Organization, slug=slug)
+
+    if request.method == "POST":
+        target_column = request.POST.get("target_column")
+        feature_columns = request.POST.getlist("feature_columns")
+
+        data = process_csv_data(organization.csv_file.path)
+        result = calculate_random_forest(data, target_column, feature_columns)
+
+        context = {
+            "organization": organization,
+            "train_r2_score": result["train_r2_score"],
+            "test_r2_score": result["test_r2_score"],
+            "train_mse": result["train_mse"],
+            "test_mse": result["test_mse"],
+            "feature_importances": zip(feature_columns, result["feature_importances"]),
+            "trees": result["trees"],
+            "feature_importances_plot": result["feature_importances_plot"],
+            "model_filename": result["model_filename"],
+            'title': 'СЦ РЭУ филиал им. Г.В. Плеханова',
+        }
+        return render(request, "ai/organization_result.html", context)
+
+    data = process_csv_data(organization.csv_file.path)
+    columns = data.columns
+
+    return render(request, "ai/organization_detail.html", {
+        "organization": organization,
+        "columns": columns,
+        'title': 'СЦ РЭУ филиал им. Г.В. Плеханова',
+    })
+
+
 def shlrr(request):
     shlrrs = SHLRR.objects.all()
 
