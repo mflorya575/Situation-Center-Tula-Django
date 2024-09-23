156caf73674c83afef03b9f352d6c8175fd760d3
Michael Florya <mih2303@mail.ru>
2024-09-23 15:31:55 +0300

    create page finance detail

--- situation_center/ai/urls.py
+++ situation_center/ai/urls.py
@@ -119,6 +119,7 @@ urlpatterns = [
     path('infotechnology/<slug:slug>/', views.infotechnology_view, name='infotechnology_view'),
     # Финансы
     path('finance/', views.finance, name='finance'),
+    path('finance/<slug:slug>/', views.finance_view, name='finance_view'),
     # Внешняя торговля
     path('foreigntrading/', views.foreigntrading, name='foreigntrading'),
     # Валовой региональный продукт
--- situation_center/ai/views.py
+++ situation_center/ai/views.py
@@ -1689,6 +1689,40 @@ def finance(request):
     return render(request, 'ai/finance.html', context)
 
 
+def finance_view(request, slug):
+    finance = get_object_or_404(Finance, slug=slug)
+
+    if request.method == "POST":
+        target_column = request.POST.get("target_column")
+        feature_columns = request.POST.getlist("feature_columns")
+
+        data = process_csv_data(finance.csv_file.path)
+        result = calculate_random_forest(data, target_column, feature_columns)
+
+        context = {
+            "finance": finance,
+            "train_r2_score": result["train_r2_score"],
+            "test_r2_score": result["test_r2_score"],
+            "train_mse": result["train_mse"],
+            "test_mse": result["test_mse"],
+            "feature_importances": zip(feature_columns, result["feature_importances"]),
+            "trees": result["trees"],
+            "feature_importances_plot": result["feature_importances_plot"],
+            "model_filename": result["model_filename"],
+            'title': 'СЦ РЭУ филиал им. Г.В. Плеханова',
+        }
+        return render(request, "ai/finance_result.html", context)
+
+    data = process_csv_data(finance.csv_file.path)
+    columns = data.columns
+
+    return render(request, "ai/finance_detail.html", {
+        "finance": finance,
+        "columns": columns,
+        'title': 'СЦ РЭУ филиал им. Г.В. Плеханова',
+    })
+
+
 def foreigntrading(request):
     foreigntradings = ForeignTrading.objects.all()
 
